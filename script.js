3f29e4c6594d0bb12eefde9e9dda83d6c3d5e22b
knika-starkey <knika787@gmail.com>
2023-04-01 02:51:58 +0300

    try activate buttons, but i'm stupid

--- script.js
+++ script.js
@@ -30,15 +30,16 @@ function loadVinyls() {
 function insertVinyls(vinyls) {
   let str = `<div class="row">`;
   for (let i = 0; i < vinyls.length; i++) {
-    str += `<div class="col-6 col-sm-4 col-lg-2 col-md-3">`;
+    str += `<div class="col-6 col-sm-4 col-lg-2 col-md-3 item_box">`;
     str += `<div class="card">`;
     str += `<img class="card-img-top" src="${vinyls[i].imageCover}" alt="Card image cap">`;
     str += `<div class="card-body">`;
-    str += `<h5 class="card-title">${vinyls[i].name}</h5>`;
+    str += `<h5 class="card-title item_title">${vinyls[i].name}</h5>`;
     str += `<p class="card-text">${vinyls[i].author}</p>`;
-    if (vinyls[i].stocked) str += `<p><strong>${vinyls[i].price}</strong></p>`;
-    else str += `<p><s>${vinyls[i].price}</s></p>`;
-    str += `<a href="#" class="btn btn-danger">Купити</a>`;
+    // if (vinyls[i].stocked)
+    str += `<p><span class="item_price">${vinyls[i].price}</span></p>`;
+    // else str += `<p><s><span>${vinyls[i].price}</span></s></p>`;
+    str += `<a class="btn btn-danger add_item" data-id="${vinyls[i].id}">Купити</a>`;
     str += `</div>`;
     str += `</div>`;
     str += `</div>`;
@@ -48,3 +49,39 @@ function insertVinyls(vinyls) {
   document.getElementById("vinyls").innerHTML = str;
 }
 loadVinyls();
+
+// let itemBox = document.getElementsByClassName("item_box"); // блок каждого товара
+
+// // Записываем данные в LocalStorage
+// function setCartData(o) {
+//   localStorage.setItem("cart", JSON.stringify(o));
+// }
+// // Получаем данные из LocalStorage
+// function getCartData() {
+//   return JSON.parse(localStorage.getItem("cart"));
+// }
+// function addToCart(e) {
+//   let button = e.target;
+//   button.disabled = true; // блокируем кнопку на время операции с корзиной
+//   let cartData = getCartData() || {}; // получаем данные корзины или создаём новый объект, если данных еще нет
+//   let parentBox = button.parentNode; // родительский элемент кнопки "Добавить в корзину";
+//   let itemId = button.getAttribute("data-id"); // ID товара
+//   let itemTitle = parentBox.querySelector(".item_title").innerHTML; // название товара
+//   let itemPrice = parentBox.querySelector(".item_price").innerHTML; // стоимость товара
+//   console.log(cartData);
+//   if (cartData.hasOwnProperty(itemId)) {
+//     cartData[itemId][2] += 1;
+//   } else {
+//     cartData[itemId] = [itemTitle, itemPrice, 1];
+//   }
+
+//   setCartData(cartData);
+//   button.disabled = false;
+// }
+// console.log(itemBox);
+
+// for (let i = 0; i < itemBox.length; i++) {
+//   itemBox[i]
+//     .querySelector('[id^="add_item"]')
+//     .addEventListener("click", addToCart);
+// }
