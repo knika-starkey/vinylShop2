b3953762c2d997c13d5effc460baf6e5ab830584
knika-starkey <knika787@gmail.com>
2023-04-22 02:28:48 +0300

    svg, change video and unsuccessful basket

--- script.js
+++ script.js
@@ -30,62 +30,40 @@ function loadVinyls() {
 function insertVinyls(vinyls) {
   let str = `<div class="row">`;
   for (let i = 0; i < vinyls.length; i++) {
-    str += `<div class="col-6 col-sm-4 col-lg-2 col-md-3 item_box">`;
-    str += `<div class="card">`;
-    str += `<img class="card-img-top" src="./images/lazy_load_image.gif"
+    str += `<div class="col-6 col-sm-4 col-lg-2 col-md-3">`;
+    str += `<div class="panel panel-default text-center item_box">`;
+    str += `<div class="panel-heading"><img class="card-img-top" src="./images/lazy_load_image.gif"
     data-src="${vinyls[i].imageCover}" alt="Card image cap">`;
-    str += `<div class="card-body">`;
-    str += `<h5 class="card-title item_title">${vinyls[i].name}</h5>`;
-    str += `<p class="card-text">${vinyls[i].author}</p>`;
-    // if (vinyls[i].stocked)
-    str += `<p><span class="item_price">${vinyls[i].price}</span></p>`;
+    str += `<h3 class="item_title lead">${vinyls[i].name}</h3>`;
+    str += `<p class="card-text ">${vinyls[i].author}</p></div>`;
+    str += `<div class="panel-body">`;
+
+    str += `<div class="panel-footer">`;
+    // str += `<h5 class="card-title item_title">${vinyls[i].name}</h5>`;
+    str += `<h6><span class="item_price">${vinyls[i].price}</span> $</h6>`;
+
+    // if (vinyls[i].stocked)btn btn-danger add_item
+
     // else str += `<p><s><span>${vinyls[i].price}</span></s></p>`;
-    str += `<a class="btn btn-danger add_item" data-id="${vinyls[i].id}">Купити</a>`;
+    str += `<button class="btn btn-danger add_item" data-id="${vinyls[i].id}">Купити</button></div>`;
     str += `</div>`;
-    str += `</div>`;
+    str += `
+  </div>`;
     str += `</div>`;
   }
 
-  str += `</div>`;
+  str += `</div><div class="text-center">
+  <button id="checkout" class="btn btn-lg btn-danger">
+    Оформить заказ
+  </button>
+  &nbsp;
+  <button id="clear_cart" class="btn btn-lg btn-danger">
+    Очистить корзину
+  </button>
+</div></div></div>`;
   document.getElementById("vinyls").innerHTML = str;
 }
 loadVinyls();
-
-// let itemBox = document.getElementsByClassName("item_box"); // блок каждого товара
-
-// // Записываем данные в LocalStorage
-// function setCartData(o) {
-//   localStorage.setItem("cart", JSON.stringify(o));
-// }
-// // Получаем данные из LocalStorage
-// function getCartData() {
-//   return JSON.parse(localStorage.getItem("cart"));
-// }
-// function addToCart(e) {
-//   let button = e.target;
-//   button.disabled = true; // блокируем кнопку на время операции с корзиной
-//   let cartData = getCartData() || {}; // получаем данные корзины или создаём новый объект, если данных еще нет
-//   let parentBox = button.parentNode; // родительский элемент кнопки "Добавить в корзину";
-//   let itemId = button.getAttribute("data-id"); // ID товара
-//   let itemTitle = parentBox.querySelector(".item_title").innerHTML; // название товара
-//   let itemPrice = parentBox.querySelector(".item_price").innerHTML; // стоимость товара
-//   console.log(cartData);
-//   if (cartData.hasOwnProperty(itemId)) {
-//     cartData[itemId][2] += 1;
-//   } else {
-//     cartData[itemId] = [itemTitle, itemPrice, 1];
-//   }
-
-//   setCartData(cartData);
-//   button.disabled = false;
-// }
-// console.log(itemBox);
-
-// for (let i = 0; i < itemBox.length; i++) {
-//   itemBox[i]
-//     .querySelector('[id^="add_item"]')
-//     .addEventListener("click", addToCart);
-// }
 
 function isVisible(elem) {
   let coords = elem.getBoundingClientRect();
@@ -112,3 +90,61 @@ function showVisible() {
 
 window.addEventListener("scroll", showVisible);
 showVisible();
+
+let itemBox = document.querySelectorAll(".item_box"); // блок каждого товара
+
+// Записываем данные в LocalStorage
+function setCartData(o) {
+  localStorage.setItem("cart", JSON.stringify(o));
+}
+// Получаем данные из LocalStorage
+function getCartData() {
+  return JSON.parse(localStorage.getItem("cart"));
+}
+function countAm() {
+  let count = 0;
+  if (getCartData()) {
+    let cartData = getCartData();
+    for (const key in cartData) {
+      count += cartData[key][2];
+    }
+  }
+  return count;
+}
+function countPr() {
+  let count = 0;
+  if (getCartData()) {
+    let cartData = getCartData();
+    for (const key in cartData) {
+      count += cartData[key][1] * cartData[key][2];
+    }
+  }
+  return count;
+}
+// Добавляем товар в корзину
+function addToCart(e) {
+  let button = e.target;
+  button.disabled = true; // блокируем кнопку на время операции с корзиной
+  let cartData = getCartData() || {}; // получаем данные корзины или создаём новый объект, если данных еще нет
+  let parentBox = button.parentNode; // родительский элемент кнопки "Добавить в корзину";
+  let itemId = button.getAttribute("data-id"); // ID товара
+  let itemTitle = parentBox.querySelector(".item_title").innerHTML; // название товара
+  let itemPrice = parentBox.querySelector(".item_price").innerHTML; // стоимость товара
+  console.log(cartData);
+  if (cartData.hasOwnProperty(itemId)) {
+    cartData[itemId][2] += 1;
+  } else {
+    cartData[itemId] = [itemTitle, itemPrice, 1];
+  }
+
+  setCartData(cartData);
+  button.disabled = false;
+}
+function clearCart(e) {
+  localStorage.removeItem("cart");
+}
+for (let i = 0; i < itemBox.length; i++) {
+  itemBox[i].querySelector(".add_item").addEventListener("click", addToCart);
+  document.getElementById("clear_cart").addEventListener("click", clearCart);
+}
+console.log(itemBox);
